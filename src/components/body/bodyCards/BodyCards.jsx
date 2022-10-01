d80267a7b5d5cb42b182018887ac776435efdd37
NikolayPilgun <a03198814s@gmail.com>
2022-10-01 18:26:22 +0500

    Backend fetch

--- src/components/body/bodyCards/BodyCards.jsx
+++ src/components/body/bodyCards/BodyCards.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import BodyCard from "./bodyCard/BodyCard";
 import styles from "./bodyCards.module.scss";
-import pizzas from "../../../assets/pizzas.json";
+//import pizzas from "../../../assets/pizzas.json";
 
 function BodyCards() {
+  //https://63382770937ea77bfdbb4510.mockapi.io/items
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://63382770937ea77bfdbb4510.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((pizzas) => {
+        setItems(pizzas);
+      });
+  }, []);
+
   return (
     <div className={styles.bodyCards}>
-      {pizzas.map((card) => (
+      {items.map((card) => (
         <BodyCard
           key={card.id}
           id={card.id}
