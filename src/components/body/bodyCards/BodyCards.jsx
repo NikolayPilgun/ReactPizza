a51632c0cdb0f2db5c0eeb9422628da5a7330952
NikolayPilgun <a03198814s@gmail.com>
2022-10-01 23:02:31 +0500

    Body Loading

--- src/components/body/bodyCards/BodyCards.jsx
+++ src/components/body/bodyCards/BodyCards.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import BodyCard from "./bodyCard/BodyCard";
 import styles from "./bodyCards.module.scss";
+import BodyLoading from "./bodyLoading/BodyLoading";
 //import pizzas from "../../../assets/pizzas.json";
 
 function BodyCards() {
   //https://63382770937ea77bfdbb4510.mockapi.io/items
   const [items, setItems] = useState([]);
+
+  const loading = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   useEffect(() => {
     fetch("https://63382770937ea77bfdbb4510.mockapi.io/items")
@@ -19,17 +22,19 @@ function BodyCards() {
 
   return (
     <div className={styles.bodyCards}>
-      {items.map((card) => (
-        <BodyCard
-          key={card.id}
-          id={card.id}
-          imageUrl={card.imageUrl}
-          title={card.title}
-          price={card.price}
-          sizes={card.sizes}
-          types={card.types}
-        />
-      ))}
+      {items.length < 1
+        ? loading.map((load) => <BodyLoading key={load} />)
+        : items.map((card) => (
+            <BodyCard
+              key={card.id}
+              id={card.id}
+              imageUrl={card.imageUrl}
+              title={card.title}
+              price={card.price}
+              sizes={card.sizes}
+              types={card.types}
+            />
+          ))}
     </div>
   );
 }
