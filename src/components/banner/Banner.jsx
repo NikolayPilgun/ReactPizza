3ca98578a94b1d2d5b3e5accfbf6f1f1ec1a4593
NikolayPilgun <a03198814s@gmail.com>
2022-10-25 22:19:45 +0500

    Revision 2

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,12 +1,12 @@
-import styles from "./banner.module.scss";
 import bannerPizza from "./banner.jpeg";
+import styles from "./banner.module.scss";
 
 function Banner() {
-  return (
-    <div className={styles.banner}>
-      <img src={bannerPizza} alt="pizza banner" />
-    </div>
-  );
+	return (
+		<div className={styles.banner}>
+			<img src={bannerPizza} alt="pizza banner" />
+		</div>
+	);
 }
 
 export default Banner;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Banner from "../banner/Banner";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 
 function Layout() {
+	const location = useLocation();
 	return (
 		<>
 			<Header />
-			<Banner />
+			{location.pathname !== "/basket" && <Banner />}
 			<Outlet />
 			<Footer />
 		</>
